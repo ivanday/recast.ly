ceb4bb036d6a4bd483bdf7bc8557c472fbcf9612
Ivan Day <ivanday@utexas.edu>
2022-11-08 11:11:30 -0600

    Implement VideoPlayer with example video data

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,13 @@
-var VideoList = () => (
-  <div className="video-list">
-    <VideoListEntry />
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos = [], setVideo}) => {
+  return (<div className="video-list">
+    {videos.map(item => {
+      return <VideoListEntry video={item} setVideo = {setVideo} />;
+    })}
+  </div>);
+};
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,7 +1,7 @@
-var VideoListEntry = ({video}) => (
-  <div className="video-list-entry media">
+var VideoListEntry = ({video, setVideo}) => (
+  <div className="video-list-entry media" onClick = {() => setVideo(video)}>
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
       <div className="video-list-entry-title">{video.snippet.title}</div>
@@ -9,6 +9,15 @@ var VideoListEntry = ({video}) => (
     </div>
   </div>
 );
+
+// VideoListEntry.videoClick = function ({handleClick}) {
+//   const [entry, setEntry] = useState({});
+
+//   return(
+
+//   )
+
+// };
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
